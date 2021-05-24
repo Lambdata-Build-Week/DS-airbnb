be14bb89f7e8515048a75eaab313d0c0fd16c8d8
BOsterbuhr <brodyosterbuhr@gmail.com>
2021-05-24 16:24:07 -0500

    Resructured package & added frontend framework

--- app/main.py
+++ app/main.py
@@ -1,8 +1,10 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
+from fastapi.staticfiles import StaticFiles
 import uvicorn
-
-from app import db, ml, viz
+from app.database import db
+from app.ml_viz import ml, viz
+from app.frontend import routes
 
 description = """
 Edit your app's title and description. See [https://fastapi.tiangolo.com/tutorial/metadata/](https://fastapi.tiangolo.com/tutorial/metadata/)
@@ -18,9 +20,12 @@ To use these interactive docs try:
 app = FastAPI(
     title='DS API',
     description=description,
-    docs_url='/',
+    docs_url='/docs',
 )
+app.mount("/assets", StaticFiles(directory="app/frontend/templates/assets"), name="assets")
+app.mount("/images", StaticFiles(directory="app/frontend/templates/images"), name="images")
 
+app.include_router(routes.router, tags=['Frontend'])
 app.include_router(db.router, tags=['Database'])
 app.include_router(ml.router, tags=['Machine Learning'])
 app.include_router(viz.router, tags=['Visualization'])
