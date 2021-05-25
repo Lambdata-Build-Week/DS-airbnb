0eee3dbdb54a4ab02c4c1ee368effcc347fea9db
RyanFikejs <83402965+RyanFikejs@users.noreply.github.com>
2021-05-25 13:57:33 -0700

    initial cleaning and pep8 for main.py

--- app/main.py
+++ app/main.py
@@ -1,35 +1,46 @@
+"""Primary file for the creation and running of this Airbnb pricing web app."""
+
+# For creating and running our web app with FastAPI
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from fastapi.staticfiles import StaticFiles
 import uvicorn
+
+# For routing between front end, back end, and databse
 from app.database import db
 from app.ml_viz import ml, viz
 from app.frontend import routes
 
-description = """
-Edit your app's title and description. See [https://fastapi.tiangolo.com/tutorial/metadata/](https://fastapi.tiangolo.com/tutorial/metadata/)
 
-To use these interactive docs try:
-- Click on an endpoint below
-- Click the **Try it out** button
-- Edit the Request body or any parameters
-- Click the **Execute** button
-- Scroll down to see the Server response Code & Details
-"""
-
+# Instantiate fastAPI with appropriate descriptors
 app = FastAPI(
-    title='DS API',
-    description=description,
-    docs_url='/docs',
+    title='Airbnb Pricing API',
+    description="Interactive pricing tool for Airbnb hosts",
+    version="1.0",
+    docs_url='/docs'
 )
-app.mount("/assets", StaticFiles(directory="app/frontend/templates/assets"), name="assets")
-app.mount("/images", StaticFiles(directory="app/frontend/templates/images"), name="images")
 
-app.include_router(routes.router, tags=['Frontend'])
-app.include_router(db.router, tags=['Database'])
-app.include_router(ml.router, tags=['Machine Learning'])
-app.include_router(viz.router, tags=['Visualization'])
 
+# Create access to static files on the page
+app.mount("/assets",
+          StaticFiles(directory="app/frontend/templates/assets"),
+          name="assets"
+          )
+
+app.mount("/images",
+          StaticFiles(directory="app/frontend/templates/images"),
+          name="images"
+          )
+
+
+# Connect to the routing utilized in the other files of the app
+app.include_router(routes.router, tags=['Frontend'])  # for routing user inputs
+app.include_router(db.router, tags=['Database'])  # interactions with database
+app.include_router(ml.router, tags=['Machine Learning'])  # predictive model
+app.include_router(viz.router, tags=['Visualization'])  # interactive content
+
+
+# Allow access between the front end and back end from different origins
 app.add_middleware(
     CORSMiddleware,
     allow_origins=['*'],
@@ -37,6 +48,7 @@ app.add_middleware(
     allow_methods=['*'],
     allow_headers=['*'],
 )
+
 
 if __name__ == '__main__':
     uvicorn.run(app)
