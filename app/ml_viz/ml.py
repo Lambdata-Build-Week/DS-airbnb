a2a4e8f0e656b6cd15c4cba87a051e265f13238e
RyanFikejs <83402965+RyanFikejs@users.noreply.github.com>
2021-05-27 01:29:25 -0700

    cleaned, commented, and checked pep8 on ml.py (still has odd indentation error), deleted routes.py

--- app/ml_viz/ml.py
+++ app/ml_viz/ml.py
@@ -1,58 +1,71 @@
-"""Machine learning functions"""
+"""Machine learning functions / price recommendation tool"""
 
-# import logging
+# Import package to "unpickle" the predictive model
 import joblib
 
+# Import packages for routing / HTML support
 from fastapi import APIRouter, Request, Form
 from fastapi.templating import Jinja2Templates
+
+# Import packages for predictive model and result dependencies
 import pandas as pd
 import numpy as np
-from pydantic import BaseModel, Field, validator
 
-# log = logging.getLogger(__name__)
+
+# Instatiate the router
 router = APIRouter()
 
+# Instantiate templates
 templates = Jinja2Templates(directory="app/frontend/templates/")
 
+# Instatiate the predictive model
 model = joblib.load("app/ml_viz/model.joblib")
 
 
-# class Dict(BaseModel):
-#     """Use this data model to parse the request body JSON."""
+# Setup the prediction process (how the data will be passed to the model)
+def predict(
+    property_type, room_type, accommodates, bathrooms, bedrooms, beds, city):
+    """Predict the best value for the Airbnb host's property based on
+    specific features found in historical data.
 
-#     # x1: float = Field(..., example=3.14)
-#     # x2: int = Field(..., example=-42)
-#     # x3: str = Field(..., example='banjo')
-#     city: str=Form(...),
-#     beds: int=Form(...),
-#     bedrooms: int=Form(...),
-#     bathrooms: int=Form(...),
-#     accommodates: int=Form(...),
-#     property_type: str=Form(...),
-#     room_type: str=Form(...)
+    Parameters are the selected features of most importance (based on
+    EDA and model testing).
 
-#     def to_df(self):
-#         """Convert pydantic object to pandas dataframe with 1 row."""
-#         return pd.DataFrame([dict(self)])
+    Returns a result in dollar amount.
+    """
 
-#     @validator('x1')
-#     def x1_must_be_positive(cls, value):
-#         """Validate that x1 is a positive number."""
-#         assert value > 0, f'x1 == {value}, must be > 0'
-#         return value
+    # Pass the arguments into a dataframe to be passed into predictive model
+    df = pd.DataFrame(columns=["property_type",
+                               "room_type",
+                               "accommodates",
+                               "bathrooms",
+                               "bedrooms",
+                               "beds",
+                               "city"
+                               ],
+                      data=[[property_type,
+                             room_type,
+                             accommodates,
+                             bathrooms,
+                             bedrooms,
+                             beds,
+                             city
+                             ]]
+                      )
 
-
-def predict(property_type, room_type, accommodates, bathrooms, bedrooms, beds, city):
-    df = pd.DataFrame(columns=["property_type", "room_type", "accommodates", "bathrooms", "bedrooms", "beds", "city"],
-    data=[[property_type, room_type, accommodates, bathrooms, bedrooms, beds, city]])
+    # Generate a prediction based on the information in the dataframe
     y_pred = model.predict(df)[0][0]
-    result = np.exp(y_pred)
-    return np.round(result, 2)
 
-# @router.get('/prediction', response_class=HTMLResponse)
+    # Revert prediction logarithmic values and round for cents
+    result = np.round(np.exp(y_pred), 2)
+
+    return f"${result} per night"
+
+
+# Route the inputs from the HTML form into the predictive model
 @router.post('/prediction')
 def echo(
-    request: Request, 
+    request: Request,
     city: str=Form(...),
     beds: int=Form(...),
     bedrooms: int=Form(...),
@@ -62,30 +75,43 @@ def echo(
     room_type: str=Form(...)
     ):
     """Gets the input data from predict.html (with respective dtypes
-    included) and returns them in JSON format."""
-    prediction = predict(property_type, room_type, accommodates, bathrooms, bedrooms, beds, city)
-    return templates.TemplateResponse('prediction.html', {"request": request, 
-                                                            "prediction": prediction,
-                                                            "property_type": f'Property type: {property_type}', 
-                                                            "room_type": f'Room type: {room_type}', 
-                                                            "accommodates": f'Accommodate: {accommodates}', 
-                                                            "bathrooms": f'Bathroom: {bathrooms}', 
-                                                            "bedrooms": f'Bedroom: {bedrooms}', 
-                                                            "beds": f'Bed: {beds}', 
-                                                            "city": f'City: {city}'})
+    included) and passes them into the predict function (used as a
+    helper function).
 
+    Parameters are the request (cleverly utilized by Minh Nuyen to
+    pass into the response template) as well as values for the features
+    necessary for the prediction, collested from the HTML form.
+
+    Returns an HTML template supplied through Jinja which displays the
+    recommended price per night as well as the selections made by the
+    user (Airbnb host) to make the reccomendation.
+    """
+
+    # Make the prediction
+    prediction = predict(property_type,
+                         room_type,
+                         accommodates,
+                         bathrooms,
+                         bedrooms,
+                         beds,
+                         city
+                         )
+
+    return templates.TemplateResponse('prediction.html',
+                                      {"request": request,
+                                       "prediction": prediction,  # modiy?
+                                       "property_type": property_type,
+                                       "room_type": f'Room type: {room_type}',
+                                       "accommodates": \
+                                       f'Accommodate: {accommodates}',
+                                       "bathrooms": f'Bathroom: {bathrooms}',
+                                       "bedrooms": f'Bedroom: {bedrooms}',
+                                       "beds": f'Bed: {beds}',
+                                       "city": f'City: {city}'
+                                       })
+
+
+# Route for display of the prediction page
 @router.get('/prediction')
 def display_index(request: Request):
     return templates.TemplateResponse('prediction.html', {"request": request})
-
-
-# @router.post('/prediction')
-# async def predict(property_type, room_type, accommodates, bathrooms, bedrooms, beds, city):
-#     df = pd.DataFrame(columns=["property_type", "room_type", "accommodates", "bathrooms", "bedrooms", "beds", "city"],
-#     data=[[property_type, room_type, accommodates, bathrooms, bedrooms, beds, city]])
-#     y_pred = model.predict(df)[0][0]
-#     result = np.exp(y_pred)
-#     return np.round(result, 2)
-
-# def display_index(request: Request, prediction=predict("Apartment", "Private room", 5, 2, 3, 3, "Austin")):
-#     return templates.TemplateResponse('prediction.html', {"request": request, "prediction": prediction})
